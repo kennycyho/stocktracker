b0f791200a5ab638dcd9f48384c7214b6a9d53ea
Kenny Ho <kennycyho@gmail.com>
2026-06-30 15:33:00 -0700

    fix imports

--- src/test/java/app/cooldown/CooldownServiceIntegrationTests.java
+++ src/test/java/app/cooldown/CooldownServiceIntegrationTests.java
@@ -17,9 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
