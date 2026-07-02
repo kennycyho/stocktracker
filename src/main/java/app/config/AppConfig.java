861ef2c39c6861ed8ee5372187021aa5ced8f124
Kenny Ho <kennycyho@gmail.com>
2026-07-02 15:02:06 -0700

    move RestClient bean into AppConfig

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
 import java.util.List;
@@ -62,5 +63,10 @@ public class AppConfig {
                         .orElseThrow(() -> new IllegalArgumentException("Unknown checker: " + c.checker()))
                         .apply(c))
                 .toList();
+    }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
     }
 }
